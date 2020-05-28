6c67c4232abe3da91b9386617386ed14480ca5d2
root <root@192.168.1.11>
2020-05-28 18:59:44 +0700

    minor lagi

--- routes/model_user.py
+++ routes/model_user.py
@@ -22,7 +22,7 @@ class UserBase(BaseModel):
     createTime: datetime = None
     updateTime: datetime = None
     nama: str = None
-    nohp: Optional[str]
+    nohp: str = None
     email: str = None
     username: str = None
     password: str = None
